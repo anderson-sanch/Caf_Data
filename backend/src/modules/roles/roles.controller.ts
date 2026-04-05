81c606b86242d1f4904b1974d7cc7871b4f4fed4
anderson sanchez <anderson.asm201@gmail.com>
2026-04-04 20:24:20 -0500

    roles y permisos

--- backend/src/modules/roles/roles.controller.ts
+++ backend/src/modules/roles/roles.controller.ts
@@ -1,19 +1,29 @@
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, Patch } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/CreateRole.dto';
-
+import { permission } from 'process';
 
 @Controller('roles')
 export class RolesController {
-    constructor(private rolesService: RolesService){}
-    // creacion
-    @Post()
-    create(@Body() dto:CreateRoleDto){
-        return this.rolesService.create(dto);
-    }
+  constructor(private rolesService: RolesService) {}
+  // creacion
+  @Post()
+  create(@Body() dto: CreateRoleDto) {
+    return this.rolesService.create(dto);
+  }
 
-    @Get()
-    findAll(){
-        return this.rolesService.findAll();
-    }
+  @Get()
+  findAll() {
+    return this.rolesService.findAll();
+  }
+
+  @Post(':id/permissions')
+  addPermission(
+    @Param('id') roleId: string,
+    @Body('permissionIds') permissionIds: string[],
+  ) {
+    console.log(permissionIds);
+    
+    return this.rolesService.addPermissions(roleId, permissionIds);
+  }
 }
--- backend/src/modules/roles/roles.service.ts
+++ backend/src/modules/roles/roles.service.ts
@@ -4,34 +4,54 @@ import { CreateRoleDto } from './dto/CreateRole.dto';
 
 @Injectable()
 export class RolesService {
-    constructor(private prisma: PrismaService){}
+  constructor(private prisma: PrismaService) {}
 
-    async create(dto:CreateRoleDto){
-        const nameCapitalized = dto.name.charAt(0).toLocaleUpperCase() + dto.name.slice(1).toLocaleLowerCase();
+  async create(dto: CreateRoleDto) {
+    const nameCapitalized =
+      dto.name.charAt(0).toLocaleUpperCase() +
+      dto.name.slice(1).toLocaleLowerCase();
 
-        const roles = await this.prisma.roles.findUnique({
-            where: {
-                name: nameCapitalized
-            }
-        });
-        
-        if(roles){
-            throw new BadRequestException('El rol ya existe');
-        }
+    const roles = await this.prisma.roles.findUnique({
+      where: {
+        name: nameCapitalized,
+      },
+    });
 
-        return this.prisma.roles.create({
-            data:{
-                name: nameCapitalized,
-                description: dto.description
-            }
-        })
+    if (roles) {
+      throw new BadRequestException('El rol ya existe');
     }
 
-    async findAll(){
-        return this.prisma.roles.findMany({
-            orderBy: {
-                name: 'asc'
-            }
-        })
+    return this.prisma.roles.create({
+      data: {
+        name: nameCapitalized,
+        description: dto.description,
+      },
+    });
+  }
+
+  async findAll() {
+    return this.prisma.roles.findMany({
+      orderBy: {
+        name: 'asc',
+      },
+    });
+  }
+
+  async addPermissions(roleId: string, permissionIds: string[]) {
+    const result = await this.prisma.role_permissions.createMany({
+      data: permissionIds.map((permissionId) => ({
+        role_id: roleId,
+        permission_id: permissionId,
+      })),
+      skipDuplicates: true,
+    });
+
+    if(result.count === 0) {
+        throw new BadRequestException('No se pudieron agregar los permisos al rol');
     }
+
+    return {
+        message: 'Permisos agregados al rol exitosamente',
+    }
+  }
 }
