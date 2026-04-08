f27e21605e697b68962ec6b095c443f87050bf41
SebasAlfa <sa7874363@gmail.com>
2026-04-08 01:43:38 -0500

    Terminacion de pagina iformativa

--- frontend/src/pages/landingPage.jsx
+++ frontend/src/pages/landingPage.jsx
@@ -1,6 +1,6 @@
-import cafe from "../imagenes/CAFE_MENU.jpg";
+import cafe from "../imagenes/CAFE_MENU..jpg";
 import comida from "../imagenes/COMIDA_MENU.jpg";
-import malteada from "../imagenes/Maldeada.jpg";
+import malteada from "../imagenes/frape.webp";
 import tasa from "../imagenes/tasa de cafe.jpg";
 
 
@@ -8,15 +8,30 @@ function LandingPage (){
 
 return(
     <div>
+      <header className="navbar">
+         <div className="logo">CafData</div>
+         <nav>
+           <ul>
+             <li><a href="#">Inicio</a></li>
+             <li><a href="#">Menú</a></li>
+             <li><a href="#">Sobre nosotros</a></li>
+             <li><a href="#">Contacto</a></li>
+           </ul>
+         </nav>
+        <button className="login-buttom">Iniciar</button>
+      </header>
+
+
         <section className="hero">
+            <div className="hero-content">
             <h1>El cafe perfecto para el mejor momento</h1>
             <p>Disfuta de una experiencia única en nuestra cafetéria acogedora.</p>
-            <button type="button">Menu</button>
+            <button type="button">Ver menu</button>
+            </div>
         </section>
 
         <section className="menu-section">
-           <h2>Disfrurta nuestro <span>delicioso</span>menu
-           </h2>
+           <h2>Disfrurta nuestro delicioso menu</h2>
            
          <div className="cards">
 
@@ -38,26 +53,26 @@ return(
               <img src={malteada} />
                <h3>Ofertas especiales</h3>
                <p>Promociones y descuentosa exclusivos para ti.</p>
-               <button type="button">Ver ofertad</button>
+               <button type="button">Ver ofertas</button>
           </div>
           
          </div>
         
         </section>
         <section className="highlight">
-          <div>
-            <h2>caramel Macchiato</h2>
+          <div className="highlight-text">
+            <h2>Prueba nuestro Caramel Macchiato</h2>
             <p>Una deliciosa mezcla de espreso,leche vaporizada y caramelo con miel.</p>
             <button>Ordenar ahora</button>
           </div>
 
-          <div className=".highlight-image">
-            <img src={tasa}  />
-          </div>
+          <div className="highlight-image">
+            <img src={tasa} alt="Caramel Macchiato" />
+        </div>
         </section>
 
         <section className="final-cta">
-          <div>
+          <div className="final-overlay">
             <h2>Vive la magia del cafe</h2>
             <p>Un lugar perfecto para trabajar o disfrutar de un buen momento con un cafe </p>
             <button>Como llegar</button>
